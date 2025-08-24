c06b1f8464863f64a1b75f1a9e6d4b3d938655c6
Sridhana Ankathi <sridhanaankathi2003@gmail.com>
2025-08-24 15:00:23 +0530

    Time: 1 ms (99.84%) | Memory: 46.6 MB (10.65%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,31 +1,9 @@
 class Solution {
-    public int singleNumber(int[] arr) {
-        int n = arr.length;
-
-        // Find minimum and maximum element
-        int mini = arr[0], maxi = arr[0];
-        for (int i = 0; i < n; i++) {
-            mini = Math.min(mini, arr[i]);
-            maxi = Math.max(maxi, arr[i]);
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;  // cancels out duplicates
         }
-
-        // Size of hash array = range of numbers
-        int size = maxi - mini + 1;
-        int[] hash = new int[size];
-
-        // Hash the given array (shifted by mini)
-        for (int i = 0; i < n; i++) {
-            hash[arr[i] - mini]++;
-        }
-
-        // Find the single element
-        for (int i = 0; i < n; i++) {
-            if (hash[arr[i] - mini] == 1) {
-                return arr[i];
-            }
-        }
-
-        // Should never reach here for valid input
-        return -1;
+        return xor;
     }
 }
